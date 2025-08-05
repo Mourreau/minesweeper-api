6acd676c53aadd596b37d1b89e5f11cfbdf26941
Moreau <max.mourreau@yandex.ru>
2025-08-05 17:35:25 +0500

    refactor(core): Изменить уровень доступа у публичных свойств чтобы нельзя было изменить их значения снаружи.

--- Minesweeper.Core/Models/Cell.cs
+++ Minesweeper.Core/Models/Cell.cs
@@ -5,21 +5,21 @@ namespace Minesweeper.Core.Models
     {
 
 
-        public bool IsMine { get; set; } = false; // Клетка является Миной
-        public bool IsFlagged { get; set; } = false; // Поставлен ли на клетку Флаг
-        public bool IsRevealed { get; set; }  = false; // Открыта ли клетка
-        public int AdjacentMinesCount { get; set; } = 0; // Количетво Мин вокруг клетки. 
+        public bool IsMine { get; internal set; } = false; // Клетка является Миной
+        public bool IsFlagged { get; internal set; } = false; // Поставлен ли на клетку Флаг
+        public bool IsRevealed { get; internal set; }  = false; // Открыта ли клетка
+        public int AdjacentMinesCount { get; internal set; } = 0; // Количество Мин вокруг клетки. 
 
 
         /// <summary>
         /// Координата X клетки на игровом поле
         /// </summary>
-        public int X { get; set; }
+        public int X { get; private set; }
 
         /// <summary>
         /// Координата Y клетки на игровом поле
         /// </summary>
-        public int Y { get; set; }
+        public int Y { get; private set; }
 
         public Cell(int x, int y)
         {
