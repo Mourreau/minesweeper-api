ca85ef1d308f18a001a208e2f44a0afc9e372020
Moreau <max.mourreau@yandex.ru>
2025-08-07 17:58:07 +0500

    refactor(app): Изменить наименование свойства Difficulty на GameStatus для большей наглядности.

--- Minesweeper.Application/DTO/GameStateDto.cs
+++ Minesweeper.Application/DTO/GameStateDto.cs
@@ -2,7 +2,7 @@ namespace Minesweeper.Application.DTO;
 
 public class GameStateDto
 {
-    public string Difficulty { get; set; }
+    public string GameStatus { get; set; }
     public List<List<CellDto>> GameBoard { get; set; }
     public int Width { get; set; }
     public int Height { get; set; }
