82571e65a21e0178998e91e19547b8f5a4677027
Moreau <max.mourreau@yandex.ru>
2025-08-07 18:10:26 +0500

    feat(api): Реализовать GET метод контроллера GameController.cs.

--- Minesweeper.Presentation/Controllers/GameController.cs
+++ Minesweeper.Presentation/Controllers/GameController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Minesweeper.Application.DTO;
 using Minesweeper.Application.Interfaces;
+using Minesweeper.Application.Mappers;
 
 namespace Minesweeper.Presentation.Controllers;
 
@@ -11,34 +12,39 @@ public class GameController : ControllerBase
     private readonly ILogger<GameController> _logger;
     private readonly IGameSessionService _gameSessionService;
     private readonly IGameManagerService _gameManagerService;
-    
+    private readonly GameStateDtoMapper _mapper;
+
     public GameController(
-        ILogger<GameController> logger, 
-        IGameSessionService gameSessionService, 
-        IGameManagerService gameManagerService)
+        ILogger<GameController> logger,
+        IGameSessionService gameSessionService,
+        IGameManagerService gameManagerService,
+        GameStateDtoMapper mapper)
     {
         _logger = logger;
         _gameSessionService = gameSessionService;
         _gameManagerService = gameManagerService;
+        _mapper = mapper;
     }
 
     [HttpPost]
     public IActionResult CreateNewGame([FromBody] NewGamePresetRequest request)
     {
         var result = _gameManagerService.CreateNewGame(request);
-        
+
         if (result.IsFailed)
             return BadRequest(result.Errors);
 
-        return CreatedAtAction(nameof(GetGameById), new {id = result.Value}, null);
+        return CreatedAtAction(nameof(GetGameById), new { id = result.Value }, null);
     }
 
     [HttpGet("{id:guid}")]
     public IActionResult GetGameById(Guid id)
     {
-        throw new Exception();
+        var result = _mapper.MapGameStateDto(id);
+
+        if (result.IsFailed)
+            return NotFound(result.Errors);
+
+        return Ok(result.Value);
     }
-
-
-    
 }
