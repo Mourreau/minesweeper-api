212a06bdb48cea56c8836b486f223b33ac1e93f8
Moreau <max.mourreau@yandex.ru>
2025-08-07 17:58:52 +0500

    refactor(app): Изменить названия свойств x,y на заглавные буквы.

--- Minesweeper.Application/DTO/CellDto.cs
+++ Minesweeper.Application/DTO/CellDto.cs
@@ -2,8 +2,8 @@ namespace Minesweeper.Application.DTO;
 
 public class CellDto
 {
-    public int x { get; set; }
-    public int y { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
     public bool IsRevealed { get; set; }
     public bool IsFlagged { get; set; }
     public int? AdjacentMinesCount { get; set; }
