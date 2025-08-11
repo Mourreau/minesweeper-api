9608a9216cb215a2801401c7d1dfd6bd5c07efc0
Moreau <max.mourreau@yandex.ru>
2025-08-11 18:18:29 +0500

    feat(app): Добавить методы открытия клетки и постановки/снятия флага в интерфейс IGameManagerService.cs сервиса управления игрой.

--- Minesweeper.Application/Interfaces/IGameManagerService.cs
+++ Minesweeper.Application/Interfaces/IGameManagerService.cs
@@ -6,4 +6,6 @@ namespace Minesweeper.Application.Interfaces;
 public interface IGameManagerService
 {
     Result<Guid> CreateNewGame(NewGamePresetRequest presetRequest);
+    Result<GameStateDto> RevealCell(Guid gameId, CellPositionDto cellPositionDto);
+    Result<GameStateDto> ToggleFlags(Guid gameId, CellPositionDto cellPositionDto);
 }
