9fb884474256e7bc33a9bfe6df523dbc061f9988
Moreau <max.mourreau@yandex.ru>
2025-08-12 19:20:47 +0500

    feat(app.helpers.errors): Добавить AppErrorBase
    - Базовый класс для всех кастомных ошибок через FluentValidation.

---
+++ Minesweeper.Application/Helpers/Errors/Base/AppErrorBase.cs
@@ -0,0 +1,9 @@
+using FluentResults;
+
+namespace Minesweeper.Application.Helpers.Errors.Base;
+
+public class AppErrorBase(AppErrorCode errorCode, string message) : Error(message)
+{
+    public AppErrorCode ErrorCode { get; } = errorCode;
+    
+}
