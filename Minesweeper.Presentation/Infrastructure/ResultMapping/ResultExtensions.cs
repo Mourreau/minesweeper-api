5db8db0de86791b54c9f33961dc0810619abe5c2
Moreau <max.mourreau@yandex.ru>
2025-08-16 15:15:27 +0500

    feat(app.helpers.errors): Ввести словарь для маппинга ошибки из FluentValidation к HTTP Статус-коду контроллера.

--- Minesweeper.Presentation/Infrastructure/ResultMapping/ResultExtensions.cs
+++ Minesweeper.Presentation/Infrastructure/ResultMapping/ResultExtensions.cs
@@ -2,11 +2,22 @@ using System.Runtime.InteropServices.ComTypes;
 using FluentResults;
 using Microsoft.AspNetCore.Mvc;
 using Minesweeper.Application.Helpers.Errors;
+using Minesweeper.Application.Helpers.Errors.Base;
 
 namespace Minesweeper.Presentation.Infrastructure.ResultMapping;
 
 public static class ResultExtensions
 {
+    private static readonly IReadOnlyDictionary<AppErrorCode, Func<ControllerBase, Error, IActionResult>> CodeMap =
+        new Dictionary<AppErrorCode, Func<ControllerBase, Error, IActionResult>>
+        {
+            [AppErrorCode.NotFound] = (c, e) => c.NotFound(e.Message),
+            [AppErrorCode.GameOver] = (@c, e) => c.BadRequest(e.Message),
+            [AppErrorCode.InvalidCoordinates] = (c, e) => c.BadRequest(e.Message),
+            [AppErrorCode.AlreadyRevealed] = (c, e) => c.Ok(e.Message),
+        };
+    
+    
     public static IActionResult ToActionResult<T>(this ControllerBase controller, Result<T> result)
     {
         if (result.IsSuccess)
