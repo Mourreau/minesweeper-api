bd5cf9fc386ae4856ca06e3534e9dea04d7cd2c4
Moreau <max.mourreau@yandex.ru>
2025-08-16 15:11:30 +0500

    refactor(app.helpers.errors): Сделать класс AppErrorBase абстрактным.

--- Minesweeper.Application/Helpers/Errors/Base/AppErrorBase.cs
+++ Minesweeper.Application/Helpers/Errors/Base/AppErrorBase.cs
@@ -2,7 +2,7 @@ using FluentResults;
 
 namespace Minesweeper.Application.Helpers.Errors.Base;
 
-public class AppErrorBase(AppErrorCode errorCode, string message) : Error(message)
+public abstract class AppErrorBase(AppErrorCode errorCode, string message) : Error(message)
 {
     public AppErrorCode ErrorCode { get; } = errorCode;
     
