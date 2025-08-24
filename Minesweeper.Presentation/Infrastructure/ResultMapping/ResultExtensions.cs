9e43da09025632e9f20065c6c7b58aaea1939f27
Moreau <max.mourreau@yandex.ru>
2025-08-24 23:32:58 +0500

    refactor(api): Исправить опечатки.

--- Minesweeper.Presentation/Infrastructure/ResultMapping/ResultExtensions.cs
+++ Minesweeper.Presentation/Infrastructure/ResultMapping/ResultExtensions.cs
@@ -6,18 +6,21 @@ using Minesweeper.Application.Helpers.Errors.Base;
 
 namespace Minesweeper.Presentation.Infrastructure.ResultMapping;
 
+/// <summary>
+/// Расширения для FluentResult.
+/// </summary>
 public static class ResultExtensions
 {
     private static readonly IReadOnlyDictionary<AppErrorCode, Func<ControllerBase, Error, IActionResult>> CodeMap =
         new Dictionary<AppErrorCode, Func<ControllerBase, Error, IActionResult>>
         {
             [AppErrorCode.NotFound] = (c, e) => c.NotFound(e.Message),
-            [AppErrorCode.GameOver] = (@c, e) => c.BadRequest(e.Message),
+            [AppErrorCode.GameOver] = (c, e) => c.BadRequest(e.Message),
             [AppErrorCode.InvalidCoordinates] = (c, e) => c.BadRequest(e.Message),
             [AppErrorCode.AlreadyRevealed] = (c, e) => c.Ok(e.Message),
         };
-    
-    
+
+
     public static IActionResult ToActionResult<T>(this ControllerBase controller, Result<T> result)
     {
         if (result.IsSuccess)
