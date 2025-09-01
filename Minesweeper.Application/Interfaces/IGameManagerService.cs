f8e1bd6a9040c4d218411f5107c0f3c35f411b8e
Moreau <max.mourreau@yandex.ru>
2025-09-01 19:34:45 +0500

    feat(app): Добавить новый метод CreateCustomNewGame в сервис IGameManagerService для создания кастомной игры.

--- Minesweeper.Application/Interfaces/IGameManagerService.cs
+++ Minesweeper.Application/Interfaces/IGameManagerService.cs
@@ -6,7 +6,8 @@ namespace Minesweeper.Application.Interfaces;
 
 public interface IGameManagerService
 {
-    Task<Result<Guid>> CreateNewGame(NewGamePresetRequest presetRequest, CancellationToken ct);
+    Task<Result<Guid>> CreateNewGameByPreset(NewGamePresetRequest presetRequest, CancellationToken ct);
+    Task<Result<Guid>> CreateCustomNewGame(CreateNewCustomGameRequest customRequest, CancellationToken ct);
     Task<Result<GameStateDto>> RevealCell(Guid gameId, CellPositionDto cellPositionDto, CancellationToken ct);
     Task<Result<GameStateDto>> ToggleFlag(Guid gameId, CellPositionDto cellPositionDto, CancellationToken ct);
     Task<Result<Game>> GetGameSession(Guid gameId, CancellationToken ct);
