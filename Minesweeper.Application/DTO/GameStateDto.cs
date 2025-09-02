4b8f49bdfe3b710258ed8549421ec7c07546c01f
Moreau <max.mourreau@yandex.ru>
2025-09-02 20:14:56 +0500

    feat(app): Добавить свойство Id в GameStateDto.

--- Minesweeper.Application/DTO/GameStateDto.cs
+++ Minesweeper.Application/DTO/GameStateDto.cs
@@ -2,8 +2,9 @@ namespace Minesweeper.Application.DTO;
 
 public class GameStateDto
 {
+    public Guid Id { get; set; }
     public string GameStatus { get; set; }
-    public List<List<CellDto>> GameBoard { get; set; }
     public int Width { get; set; }
     public int Height { get; set; }
+    public List<List<CellDto>> GameBoard { get; set; }
 }
