0ed44383e080ef1c5616b97b2e1aa769566a5185
Moreau <max.mourreau@yandex.ru>
2025-09-02 20:17:10 +0500

    feat(app): Добавить свойство IsMine в CellDto.

--- Minesweeper.Application/DTO/CellDto.cs
+++ Minesweeper.Application/DTO/CellDto.cs
@@ -7,4 +7,6 @@ public class CellDto
     public bool IsRevealed { get; set; }
     public bool IsFlagged { get; set; }
     public int? AdjacentMinesCount { get; set; }
+    
+    public bool? IsMine { get; set; }
 }
